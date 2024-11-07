9ed2e5223c86f4bb67ee8e386d6a7d6f589eb42b
gichan222 <>
2024-11-07 13:21:41 +0900

    chore(init) : 초기 설정 및 깃 이그노어 설정

---
+++ plankton-hackathon/src/test/java/com/example/planktonhackathon/PlanktonHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.planktonhackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanktonHackathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
