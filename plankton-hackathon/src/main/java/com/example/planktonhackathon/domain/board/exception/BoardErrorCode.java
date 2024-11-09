d40948d8c10d3928d6cbc68c591d91fa6cb8506c
gichan222 <>
2024-11-10 04:35:50 +0900

    fix : s3 로직 수정

--- plankton-hackathon/src/main/java/com/example/planktonhackathon/domain/board/exception/BoardErrorCode.java
+++ plankton-hackathon/src/main/java/com/example/planktonhackathon/domain/board/exception/BoardErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum BoardErrorCode implements ErrorCode {
-    BOARD_NOT_FOUND(HttpStatus.BAD_REQUEST, "게시글이 존재하지 않습니다.");
+    BOARD_NOT_FOUND(HttpStatus.BAD_REQUEST, "게시글이 존재하지 않습니다."),
+    CAN_NOT_WRITE(HttpStatus.BAD_REQUEST,"게시글은 하루 한 번만 작성할 수 있습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
--- plankton-hackathon/src/main/java/com/example/planktonhackathon/global/s3/service/S3Service.java
+++ plankton-hackathon/src/main/java/com/example/planktonhackathon/global/s3/service/S3Service.java
@@ -3,6 +3,8 @@ package com.example.planktonhackathon.global.s3.service;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,12 +20,15 @@ public class S3Service {
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
-    public String uploadFile(MultipartFile file, String email) throws IOException {
-        // 파일명 가져오기
-        String fileName = String.valueOf(UUID.randomUUID());
+    public String uploadFile(MultipartFile file, String nickName) throws IOException {
+        // 파일명 생성
+        String fileName = UUID.randomUUID().toString();
 
-        // S3에 업로드할 파일 URL 생성
-        String fileUrl = "https://" + bucket + "/" + email + "/" + fileName;
+        // S3에 업로드할 파일 URL 생성 (한 번 인코딩된 이메일 아래에 파일명이 들어가도록)
+        String fileUrl = "https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/" + nickName + "/" + fileName;
+
+        // 파일 경로는 한 번 인코딩된 이메일 폴더 아래 파일명으로 설정
+        String s3FilePath = nickName + "/" + fileName;
 
         // 메타데이터 설정
         ObjectMetadata metadata = new ObjectMetadata();
@@ -31,7 +36,7 @@ public class S3Service {
         metadata.setContentLength(file.getSize());
 
         // 파일을 S3에 업로드
-        amazonS3Client.putObject(bucket, fileName, file.getInputStream(), metadata);
+        amazonS3Client.putObject(bucket, s3FilePath, file.getInputStream(), metadata);
 
         // 업로드된 파일 URL 반환
         return fileUrl;
