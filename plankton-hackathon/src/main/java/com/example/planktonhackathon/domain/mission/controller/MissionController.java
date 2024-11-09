099c1aee3f913a1b493dfbcee85615302111218b
gichan222 <>
2024-11-09 18:44:11 +0900

    feat(Mission) : 미션 팀 auth

--- plankton-hackathon/src/main/java/com/example/planktonhackathon/domain/mission/controller/MissionController.java
+++ plankton-hackathon/src/main/java/com/example/planktonhackathon/domain/mission/controller/MissionController.java
@@ -2,6 +2,7 @@ package com.example.planktonhackathon.domain.mission.controller;
 
 import com.example.planktonhackathon.domain.mission.response.MissionResponse;
 import com.example.planktonhackathon.domain.mission.service.MissionService;
+import com.example.planktonhackathon.global.auth.annotation.Auth;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,7 @@ public class MissionController {
     private final MissionService missionService;
 
     // district와 bigCategory를 파라미터로 받아서 Mission 정보 반환
+    @Auth
     @Operation(summary = "미션 참여 및 팀에 대한 정보를 제공하는 api입니다.")
     @GetMapping("/mission")
     public ResponseEntity<MissionResponse> getMissionInfo(@RequestParam String district, @RequestParam String bigCategory) {
