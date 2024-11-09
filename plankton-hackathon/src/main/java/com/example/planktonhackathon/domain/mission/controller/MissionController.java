01e6d10e91b28e16e9b192d96af7c79ec2fc8d79
gichan222 <>
2024-11-09 18:42:42 +0900

    feat(Mission) : 미션 팀 스웨거

--- plankton-hackathon/src/main/java/com/example/planktonhackathon/domain/mission/controller/MissionController.java
+++ plankton-hackathon/src/main/java/com/example/planktonhackathon/domain/mission/controller/MissionController.java
@@ -2,12 +2,15 @@ package com.example.planktonhackathon.domain.mission.controller;
 
 import com.example.planktonhackathon.domain.mission.response.MissionResponse;
 import com.example.planktonhackathon.domain.mission.service.MissionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "미션 API", description = "미션에 대한 처리를 담당하는 API입니다")
 @RestController
 @RequiredArgsConstructor
 public class MissionController {
@@ -15,6 +18,7 @@ public class MissionController {
     private final MissionService missionService;
 
     // district와 bigCategory를 파라미터로 받아서 Mission 정보 반환
+    @Operation(summary = "미션 참여 및 팀에 대한 정보를 제공하는 api입니다.")
     @GetMapping("/mission")
     public ResponseEntity<MissionResponse> getMissionInfo(@RequestParam String district, @RequestParam String bigCategory) {
         return ResponseEntity.ok().body(missionService.getMissionInfo(district, bigCategory));
